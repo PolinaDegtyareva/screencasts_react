c2e249f4272ad3424644e6eebdafe9aaa1ca75ed
Polina <polina.degtyareva.99@gmail.com>
2022-06-16 19:01:12 +0300

    added file: fixture; changed .js: app, article, index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import {render} from "react-dom"
+import App from "./components/App";
 
-function HelloWorld() {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    )
-}
 
-render(<HelloWorld/>, document.getElementById('root'))
+
+
+render(<App/>, document.getElementById('root'))
