ee41c9e49ce9a5cea2d46c5f36abb24e1504c835
Polina <polina.degtyareva.99@gmail.com>
2022-06-19 18:03:27 +0300

    all

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,7 +5,6 @@ class Article extends PureComponent {
         super(props);
 
         this.state = {
-            isOpen: props.defaultOpen,
             count: 0
         }
     }
@@ -17,30 +16,30 @@ class Article extends PureComponent {
     componentWillMount() {
         console.log('---', 'mounting')
     }
-
-    componentWillReceiveProps(nextProps, nextContext) {
-        if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState ({
-            isOpen: nextProps.defoultOpen
-        })
-    }
-
+    //
+    // componentWillReceiveProps(nextProps, nextContext) {
+    //     if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState ({
+    //         isOpen: nextProps.defoultOpen
+    //     })
+    // }
+    //
     componentWillUpdate() {
         console.log('---', 'will update')
     }
 
 
     render() {
-        const {article} = this.props
+        const {article, isOpen, onButtonClick} = this.props
         const style = {width: '50%'}
-        const body = this.state.isOpen && <section className="card-text">{article.text}</section>
+        const body = isOpen && <section className="card-text">{article.text}</section>
         return (
             <div className="card mx-auto" style={style}>
                 <div className="card-header">
                     <h2 onClick = {this.incrementCounter}>
                         {article.title}
                         clicked {this.state.count}
-                        <button onClick={this.handleClick} className="btn bth-primary btn-lg float-right">
-                            {this.state.isOpen ? 'close' : 'open'}
+                        <button onClick={onButtonClick} className="btn bth-primary btn-lg float-right">
+                            {isOpen ? 'close' : 'open'}
                         </button>
                     </h2>
                 </div>
@@ -57,12 +56,6 @@ class Article extends PureComponent {
     incrementCounter = () => {
         this.setState({
             count: this.state.count + 1
-        })
-    }
-
-    handleClick = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
         })
     }
 }
