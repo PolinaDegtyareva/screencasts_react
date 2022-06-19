c3e8742431c6f14da8953992de6e158e49d03b0b
Polina <polina.degtyareva.99@gmail.com>
2022-06-19 16:46:03 +0300

    should/component/update

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,20 +1,24 @@
-import React, {Component} from 'react'
+import React, {Component, PureComponent} from 'react'
 
-class Article extends Component {
+class Article extends PureComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            isOpen: props.defaultOpen
+            isOpen: props.defaultOpen,
+            count: 0
         }
     }
+    //
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return this.state.isOpen !== nextState.isOpen
+    // }
 
     componentWillMount() {
         console.log('---', 'mounting')
     }
 
     componentWillReceiveProps(nextProps) {
-        console.log('---', 'will receive props')
         if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState ({
             isOpen: nextProps.defoultOpen
         })
@@ -32,8 +36,9 @@ class Article extends Component {
         return (
             <div className="card mx-auto" style={style}>
                 <div className="card-header">
-                    <h2>
+                    <h2 onClick = {this.incrementCounter}>
                         {article.title}
+                        clicked {this.state.count}
                         <button onClick={this.handleClick} className="btn bth-primary btn-lg float-right">
                             {this.state.isOpen ? 'close' : 'open'}
                         </button>
@@ -49,8 +54,13 @@ class Article extends Component {
         )
     }
 
+    incrementCounter = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
     handleClick = () => {
-        console.log('---', 'clicked')
         this.setState({
             isOpen: !this.state.isOpen
         })
